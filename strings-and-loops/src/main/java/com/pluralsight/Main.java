f533f0791dc87a798269e2d39379d70b18881fe0
poteatc <poteatc@outlook.com>
2024-10-02 11:31:42 -0400

    new examples

--- strings-and-loops/src/main/java/com/pluralsight/Main.java
+++ strings-and-loops/src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,32 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+//        String s = "Hello World";
+//        System.out.println(s.toLowerCase());
 
+        String allListItems = "Splenda,Coffee,Coke,Eggs,Cheese,Tea,Lettuce,Apples,Oranges,Bananas";
+        allListItems = allListItems.replace(",", "\n");
+        System.out.println(allListItems);
+        String[] list = allListItems.split(",");
+        System.out.println(list[0]);
+
+        String newList = """
+                    Splenda
+                        Coffee
+                            Coke
+                                Eggs
+                                    Cheese 
+                                Tea 
+                            Lettuce 
+                        Apples 
+                    Oranges 
+                        Bananas
+                 """;
+        System.out.println(newList.toLowerCase());
+        String s = """
+                |\t|\t|
+                    |\t|\t|
+                """;
+        System.out.println(s);
     }
 }
