460ad3d4df5a3d59ae9192ffd25c56d342f00624
poteatc <poteatc@outlook.com>
2024-10-02 15:04:19 -0400

    added FullNameParser for exercise 2 of workbook 2

---
+++ full-name-application/src/main/java/com/pluralsight/FullNameParser.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameParser {
+    public static void main(String[] args) {
+        System.out.print("Please enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String fullName = scanner.nextLine();
+        String[] names = fullName.split("\\s+");
+        String firstName = "(none)";
+        String middleName = "(none)";
+        String lastName = "(none)";
+        if (names.length == 2) {
+            firstName = names[0];
+            lastName = names[1];
+        }
+        if (names.length == 3) {
+            firstName = names[0];
+            middleName = names[1];
+            lastName = names[2];
+        }
+        System.out.printf("First name: %s\nMiddle name: %s\nLast name: %s", firstName, middleName, lastName);
+    }
+}
