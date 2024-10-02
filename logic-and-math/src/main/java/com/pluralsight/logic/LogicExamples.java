2f256ef8d6adf54d1d16e7e1ef278c4dbf167225
poteatc <poteatc@outlook.com>
2024-10-02 09:46:40 -0400

    Adding workbook-2 as GitHub repo

---
+++ logic-and-math/src/main/java/com/pluralsight/logic/LogicExamples.java
@@ -0,0 +1,20 @@
+package com.pluralsight.logic;
+
+public class LogicExamples {
+    public static void main(String[] args) {
+        boolean amIDone = false;
+        int i = 0;
+        do {
+            System.out.println("words...");
+            if (i++ == 5) {
+                amIDone = true;
+            }
+        } while(!amIDone);
+
+        i = 0;
+        while (i != 5) {
+            System.out.println("wordsA");
+            i++;
+        }
+    }
+}
