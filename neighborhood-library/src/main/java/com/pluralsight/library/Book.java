f92bf6dae96d7a3ef51ee4613b0915bb9b01ca41
poteatc <poteatc@outlook.com>
2024-10-07 10:35:10 -0400

    Added comments explaining code in Book.java

--- neighborhood-library/src/main/java/com/pluralsight/library/Book.java
+++ neighborhood-library/src/main/java/com/pluralsight/library/Book.java
@@ -20,6 +20,7 @@ public class Book {
         this.checkedOutTo = "";
     }
 
+    // Parametrized constructor
     public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
         this.id = id;
         this.isbn = isbn;
@@ -27,43 +28,50 @@ public class Book {
         this.isCheckedOut = isCheckedOut;
         this.checkedOutTo = checkedOutTo;
     }
-
+    // getter for id
     public int getId() {
         return id;
     }
-
+    // setter for id
     public void setId(int id) {
         this.id = id;
     }
 
+    // getter for isbn
     public String getIsbn() {
         return isbn;
     }
 
+    // setter for isbn
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
+    // getter for title
     public String getTitle() {
         return title;
     }
-
+    // setter for title
     public void setTitle(String title) {
         this.title = title;
     }
 
+    // getter for isCheckedOutTo returning if a Book is checked out
     public boolean isCheckedOut() {
         return isCheckedOut;
     }
 
+    // setter for isCheckedOut to
     public void setCheckedOut(boolean checkedOut) {
         isCheckedOut = checkedOut;
     }
 
+    // getter for isCheckedOutTo returning name of person Book is checked out to
     public String getCheckedOutTo() {
         return checkedOutTo;
     }
 
+    // setter for isCheckedOutTo setting name of person Book is checked out to
     public void setCheckedOutTo(String checkedOutTo) {
         this.checkedOutTo = checkedOutTo;
     }
